e34f7432de81af791606e50f221d3487d724993e
varsheim <lzielinski66@gmail.com>
2016-10-11 01:50:11 +0200

    Dolaczony plik h

--- inc/i2c.h
+++ inc/i2c.h
@@ -10,19 +10,35 @@
 
 #include "stm32f30x.h"
 
-#define I2Cx                          I2C1
-#define I2Cx_CLK                      RCC_APB1Periph_I2C1
-#define I2Cx_SDA_GPIO_CLK             RCC_AHBPeriph_GPIOB
-#define I2Cx_SDA_PIN                  GPIO_Pin_9
-#define I2Cx_SDA_GPIO_PORT            GPIOB
-#define I2Cx_SDA_SOURCE               GPIO_PinSource9
-#define I2Cx_SDA_AF                   GPIO_AF_I2C1
+//#define I2Cx                          I2C1
+//#define I2Cx_CLK                      RCC_APB1Periph_I2C1
+//#define I2Cx_SDA_GPIO_CLK             RCC_AHBPeriph_GPIOB
+//#define I2Cx_SDA_PIN                  GPIO_Pin_9
+//#define I2Cx_SDA_GPIO_PORT            GPIOB
+//#define I2Cx_SDA_SOURCE               GPIO_PinSource9
+//#define I2Cx_SDA_AF                   GPIO_AF_I2C1
+//
+//#define I2Cx_SCL_GPIO_CLK             RCC_AHBPeriph_GPIOB
+//#define I2Cx_SCL_PIN                  GPIO_Pin_6
+//#define I2Cx_SCL_GPIO_PORT            GPIOB
+//#define I2Cx_SCL_SOURCE               GPIO_PinSource6
+//#define I2Cx_SCL_AF                   GPIO_AF_I2C1
 
-#define I2Cx_SCL_GPIO_CLK             RCC_AHBPeriph_GPIOB
-#define I2Cx_SCL_PIN                  GPIO_Pin_6
-#define I2Cx_SCL_GPIO_PORT            GPIOB
-#define I2Cx_SCL_SOURCE               GPIO_PinSource6
-#define I2Cx_SCL_AF                   GPIO_AF_I2C1
+//#define CPAL_I2C1_SCL_GPIO_PORT         GPIOB                  (X : Name of the GPIO PORT  (A,B,C,....))
+//#define CPAL_I2C1_SCL_GPIO_CLK          RCC_APB2Periph_GPIOB   (X : Name of the GPIO PORT  (A,B,C,....))
+//#define CPAL_I2C1_SCL_GPIO_PIN          GPIO_Pin_6             (X : Pin number (1,2,3,....))
+//#define CPAL_I2C1_SCL_GPIO_PINSOURCE    GPIO_PinSource6        (X : Pin number (1,2,3,....))
+//
+//#define CPAL_I2C1_SDA_GPIO_PORT         GPIOB                  (X : Name of the GPIO PORT  (A,B,C,....))
+//#define CPAL_I2C1_SDA_GPIO_CLK          RCC_APB2Periph_GPIOB   (X : Name of the GPIO PORT  (A,B,C,....))
+//#define CPAL_I2C1_SDA_GPIO_PIN          GPIO_Pin_9             (X : Pin number (1,2,3,....))
+//#define CPAL_I2C1_SDA_GPIO_PINSOURCE    GPIO_PinSource9        (X : Pin number (1,2,3,....))
+
+
+/**************************************************************************************/
+
+
+/**************************************************************************************/
 
 void I2C_init();
 
@@ -44,7 +60,5 @@ void I2C_NVIC_Configuration(void);
 
 /**************************************************************************************/
 
-void I2C1_ER_IRQHandler(void);
-void I2C1_EV_IRQHandler(void);
 
 #endif /* I2C_H_ */
